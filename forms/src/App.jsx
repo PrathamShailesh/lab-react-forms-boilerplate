4ab060a72c92f5e8c5b58ba988c108088e3c092b
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2024-01-10 11:28:29 +0530

    new

---
+++ forms/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Form from './component/form'
+
+function App() {
+
+
+  return (
+    <>
+    <Form/>
+
+    </>
+  )
+}
+
+export default App
